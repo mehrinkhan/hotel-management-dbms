317b3c2c059c405f15a41631e9cd92ee3ce5a08c
mehrinkhan <149152829+mehrinkhan@users.noreply.github.com>
2026-04-16 23:23:50 +0600

    Update script.js

--- script.js
+++ script.js
@@ -2,27 +2,9 @@
    1. DATABASE & PERSISTENCE LAYER
    ========================================= */
 const demoRooms = [
-    {
-        id: 101,
-        name: "Skyline Executive",
-        price: 12500,
-        status: "Available",
-        img: "https://images.unsplash.com/photo-1631049307264-da0ec9d70304?auto=format&fit=crop&w=1000&q=80"
-    },
-    {
-        id: 102,
-        name: "Urban Loft",
-        price: 8500,
-        status: "Available",
-        img: "https://images.unsplash.com/photo-1590490360182-c33d57733427?auto=format&fit=crop&w=1000&q=80"
-    },
-    {
-        id: 103,
-        name: "The Royal Suite",
-        price: 25000,
-        status: "Available",
-        img: "https://images.unsplash.com/photo-1582719478250-c89cae4dc85b?auto=format&fit=crop&w=1000&q=80"
-    }
+    { id: 101, name: "Skyline Executive", price: 12500, status: "Available", img: "room1.avif" },
+    { id: 102, name: "Urban Loft", price: 8500, status: "Available", img: "room2.jpeg" },
+    { id: 103, name: "The Royal Suite", price: 25000, status: "Available", img: "room3.jpg" }
 ];
 
 
